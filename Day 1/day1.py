e60785aa770d94c43c9b24137d6ba18c27e3a7d3
Stone, Thomas {MDBT~Welwyn} <tom.stone@roche.com>
2020-12-02 10:01:39 +0000

    cleaned a little

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,8 +1,9 @@
 import os
 import time
 
+## Star 1
 star1_start = time.time()
-with open('input.txt', 'r') as fh:
+with open(os.path.join('Day 1', 'input.txt'), 'r') as fh:
     inputData = fh.read()
 input_list = [int(item) for item in inputData.split('\n') if item.isnumeric()]
 for item in input_list:
@@ -15,9 +16,9 @@ for item in input_list:
     break
 print("--- {time} seconds ---".format(time=time.time()- star1_start))
 
+##star 2
 star2_start = time.time()
 for first_item in input_list:
-    # 2020 - 1052 = 968
     remainder_1 = 2020 - first_item
     for second_item in input_list:
         remainder_2 = remainder_1 - second_item
@@ -25,9 +26,9 @@ for first_item in input_list:
             result = input_list.index(remainder_2)
         except ValueError:
             continue
-        print("{item} * {remain1} * {remain2} = {result}".format(
-            item = first_item, 
-            remain1 = second_item, 
+        print("{first_item} * {second_item} * {remain2} = {result}".format(
+            first_item = first_item, 
+            second_item = second_item, 
             remain2 = remainder_2, 
             result = first_item * second_item * remainder_2))
         break
